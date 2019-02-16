7792a456f55bacca1357b787fe35c891ebc544eb
Anh Viet Pham <pham.anh.viet.vlth@gmail.com>
2019-02-17 05:31:27 +0700

    Define lambda function using invoke method

---
+++ src/lambda/InvokeLambda.kt
@@ -0,0 +1,10 @@
+package lambda
+
+fun main(args: Array<String>) {
+    val add = {x : Int -> x + 1 }
+    println(add(4))
+
+    println("Invoke = ${add.invoke(2)}")
+
+    println("Anonymous Invocation = ${{ x: Int -> x + 4 }(2)}")
+}
