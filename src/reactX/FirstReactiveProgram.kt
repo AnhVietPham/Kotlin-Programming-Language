2cb8be734e5be92ea7e736955fbea76311709491
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-14 17:02:08 +0700

    Reactive Programming: FirstReactiveProgram.kt

---
+++ src/reactX/FirstReactiveProgram.kt
@@ -0,0 +1,29 @@
+package reactX
+
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.kotlin.toObservable
+
+fun main() {
+    val list: List<Any> = listOf("One", 2, "Three", "Four", 4.5,
+            "Five", 6.0f) // 1
+    val iterator = list.iterator() // 2
+    while (iterator.hasNext()) { // 3
+        println(iterator.next()) // Prints each element 4
+    }
+
+    println("This time with observable:")
+
+    val observable: Observable<Any> = list.toObservable()
+    observable.subscribeBy(
+            onNext = {
+                println(it)
+            },
+            onComplete = {
+                println("Done!")
+            },
+            onError = {
+                it.printStackTrace()
+            }
+    )
+}
