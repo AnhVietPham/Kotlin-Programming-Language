f0676186f8681b7b0e619cdb02f039b84f8c4ae4
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-09-23 08:55:37 +0700

    Add BuildingSequenceProgram.kt

---
+++ src/sequence-collection/sequences/BuildingSequenceProgram.kt
@@ -0,0 +1,32 @@
+package `sequence-collection`.sequences
+
+fun main() {
+    val fibonacciSeries = sequence {
+        var a = 0
+        var b = 1
+        yield(a)
+        yield(b)
+
+        while (true) {
+            val c = a + b
+            yield(c)
+            a = b
+            b = c
+        }
+    }
+
+    println(fibonacciSeries.take(10).toList())
+
+
+    fun fibonacci() = sequence {
+        var terms = Pair(0, 1)
+
+        // this sequence is infinite
+        while (true) {
+            yield(terms.first)
+            terms = Pair(terms.second, terms.first + terms.second)
+        }
+    }
+
+    println(fibonacci().take(10).toList())
+}
