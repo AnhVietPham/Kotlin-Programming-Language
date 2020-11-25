bfbf4e9a13d7a3c94b49c3aed658790d17de257b
PHAM ANH VIET <pham.anh.viet.vlth@gmail.com>
2020-11-25 10:21:02 +0700

    Demo AsyncSubjectProgram.kt

---
+++ src/reactive_programming/subjects/AsyncSubjectProgram.kt
@@ -0,0 +1,59 @@
+package reactive_programming.subjects
+
+import io.reactivex.rxjava3.subjects.AsyncSubject
+
+fun main() {
+    /*
+    * AsyncSubject will emit the last value it got, and will emit it only one time.
+    * */
+//    val observable = Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9)
+//    val subject = AsyncSubject.create<Int>()
+//    observable.subscribe(subject)
+//    subject.subscribe({
+//        //onNext
+//        println("Received $it")
+//    }, {
+//        //onError
+//        it.printStackTrace()
+//    }, {
+//        //onComplete
+//        println("Complete")
+//    })
+
+    /*
+    * On Subject instances, you can pass values directly with the onNext method, without subscribing to any Observable.
+    * */
+    val subject = AsyncSubject.create<Int>()
+    subject.onNext(1)
+    subject.onNext(2)
+    subject.onNext(3)
+    subject.onNext(4)
+    subject.onNext(5)
+    subject.onNext(6)
+    subject.subscribe({
+        println("S1 Received $it")
+    }, {
+        it.printStackTrace()
+    }, {
+        println("S1 Complete")
+    })
+    subject.onNext(7)
+    subject.onNext(8)
+    subject.subscribe({
+        println("S1 Received $it")
+    }, {
+        it.printStackTrace()
+    }, {
+        println("S1 Complete")
+    })
+    subject.onNext(9)
+    subject.subscribe({
+        println("S1 Received $it")
+    }, {
+        it.printStackTrace()
+    }, {
+        println("S1 Complete")
+    })
+    subject.onComplete()
+
+}
